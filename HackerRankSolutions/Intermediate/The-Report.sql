6cc8deefa67fe3502a199c2c8152e68a462dc671
Mano Amirth <100770935+ManonmaniAmirthalingam@users.noreply.github.com>
2024-10-18 13:48:41 +0530

    Create The-Report.sql
    Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. Ketty doesn't want the NAMES of those students who received a grade lower than 8. The report must be in descending order by grade -- i.e. higher grades are entered first. If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order. If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.

---
+++ HackerRankSolutions/Intermediate/The-Report.sql
@@ -0,0 +1,11 @@
+#Approach-1 using iif condition
+select iif(grade<8, null,name) , grade , marks
+from Students mar
+join grades grad on mar.marks between grad.min_mark and grad.max_mark
+order by grade desc, name asc
+--------------------------------------------------------------------------
+#Approach-2 using case when
+select case when grade<8 then null else name end, grade , marks
+from Students mar
+join grades grad on mar.marks between grad.min_mark and grad.max_mark
+order by grade desc, name asc
